39efe898d1ff7fcb5d60d78aa703e85f5d386e38
Pavle Sarenac <sp200359d@student.etf.bg.ac.rs>
2023-12-09 16:35:26 +0100

    /login endpoint refactored.

--- store_management_system_code/user_accounts_management/application.py
+++ store_management_system_code/user_accounts_management/application.py
@@ -14,11 +14,11 @@ application.config.from_object(Configuration)
 jwt = JWTManager(application)
 
 
-def validateRegistrationInput(postRequestBody):
-    forename = postRequestBody.json.get("forename", "")
-    surname = postRequestBody.json.get("surname", "")
-    email = postRequestBody.json.get("email", "")
-    password = postRequestBody.json.get("password", "")
+def validateRegistrationInput():
+    forename = request.json.get("forename", "")
+    surname = request.json.get("surname", "")
+    email = request.json.get("email", "")
+    password = request.json.get("password", "")
 
     if len(forename) == 0:
         return "Field forename is missing.", 400, forename, surname, email, password
@@ -43,6 +43,29 @@ def validateRegistrationInput(postRequestBody):
     return "", 0, forename, surname, email, password
 
 
+def validateLoginInput():
+    email = request.json.get("email", "")
+    password = request.json.get("password", "")
+
+    if len(email) == 0:
+        return "Field email is missing.", 400, None
+    if len(password) == 0:
+        return "Field password is missing.", 400, None
+
+    email_pattern = r'^[\w\.-]+@[\w\.-]+\.(com|org|net|edu|gov|mil|io|co\.uk)$'
+    if not re.match(email_pattern, email):
+        return "Invalid email.", 400, None
+
+    user = User.query.filter(and_(
+        User.email == email,
+        User.password == password
+    )).first()
+    if not user:
+        return "Invalid credentials.", 400, None
+
+    return "", 0, user
+
+
 def registerNewUser(userRole, forename, surname, email, password):
     userRoleId = CUSTOMER_ROLE_ID if userRole == "customer" else COURIER_ROLE_ID
     user = User(
@@ -57,12 +80,22 @@ def registerNewUser(userRole, forename, surname, email, password):
 
 
 def registration(userRole):
-    errorMessage, errorCode, forename, surname, email, password = validateRegistrationInput(request)
+    errorMessage, errorCode, forename, surname, email, password = validateRegistrationInput()
     if len(errorMessage) > 0:
         return jsonify(message=errorMessage), errorCode
     registerNewUser(userRole, forename, surname, email, password)
     return Response(status=200)
 
+
+def getAccessToken(user):
+    additionalClaims = {
+        "forename": user.forename,
+        "surname": user.surname,
+        "password": user.password,
+        "roleId": str(user.roleId)
+    }
+    return create_access_token(identity=user.email, additional_claims=additionalClaims)
+    
 
 @application.route("/register_customer", methods=["POST"])
 def registerCustomer():
@@ -76,32 +109,10 @@ def registerCourier():
 
 @application.route("/login", methods=["POST"])
 def login():
-    email = request.json.get("email", "")
-    password = request.json.get("password", "")
-
-    if len(email) == 0:
-        return jsonify(message="Field email is missing."), 400
-    if len(password) == 0:
-        return jsonify(message="Field password is missing."), 400
-
-    email_pattern = r'^[\w\.-]+@[\w\.-]+\.(com|org|net|edu|gov|mil|io|co\.uk)$'
-    if not re.match(email_pattern, email):
-        return jsonify(message="Invalid email."), 400
-
-    user = User.query.filter(and_(
-        User.email == email,
-        User.password == password
-    )).first()
-    if not user:
-        return jsonify(message="Invalid credentials."), 400
-
-    additionalClaims = {
-        "forename": user.forename,
-        "surname": user.surname,
-        "password": user.password,
-        "roleId": str(user.roleId)
-    }
-    accessToken = create_access_token(identity=user.email, additional_claims=additionalClaims)
+    errorMessage, errorCode, user = validateLoginInput()
+    if len(errorMessage) > 0:
+        return jsonify(message=errorMessage), errorCode
+    accessToken = getAccessToken(user)
     return jsonify(accessToken=accessToken), 200
 
 
