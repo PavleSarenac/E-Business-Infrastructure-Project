d74568af6a4fd26ec1eb3b47640125908186b156
Pavle Sarenac <sp200359d@student.etf.bg.ac.rs>
2023-12-10 20:23:50 +0100

    /orders_to_deliver and /pick_up_order endpoints optimized.
    Database calls minimized.

--- store_management_system_code/store_management/courierApplication.py
+++ store_management_system_code/store_management/courierApplication.py
@@ -9,46 +9,67 @@ application.config.from_object(Configuration)
 jwt = JWTManager(application)
 
 
+def validateOrdersToDeliverRequest():
+    jwtToken = get_jwt()
+    if jwtToken["roleId"] != "3":
+        return "Missing Authorization Header", 401
+    return "", 0
+
+
+def getUndeliveredOrders():
+    undeliveredOrders = {"orders": []}
+    for order in Order.query.filter(Order.orderStatus == "CREATED").all():
+        undeliveredOrders["orders"].append({
+            "id": order.id,
+            "email": order.buyerEmail
+        })
+    return undeliveredOrders
+
+
+def validatePickUpOrderRequest():
+    jwtToken = get_jwt()
+    if jwtToken["roleId"] != "3":
+        return "Missing Authorization Header", 401, None
+
+    orderId = request.json.get("id", None)
+    if orderId is None:
+        return "Missing order id.", 400, None
+    if type(orderId) is not int or orderId <= 0:
+        return "Invalid order id.", 400, None
+    orderForPickUp = Order.query.filter(Order.id == orderId).first()
+    if not orderForPickUp:
+        return "Invalid order id.", 400, None
+    if orderForPickUp.orderStatus == "PENDING" or orderForPickUp.orderStatus == "COMPLETE":
+        return "Invalid order id.", 400, None
+
+    return "", 0, orderForPickUp
+
+
+def confirmOrderPickUp(orderForPickUp):
+    orderForPickUp.orderStatus = "PENDING"
+    database.session.commit()
+
+
 @application.route("/orders_to_deliver", methods=["GET"])
 @jwt_required()
 def orders_to_deliver():
-    jwtToken = get_jwt()
-    if jwtToken["roleId"] != "3":
-        return jsonify(msg="Missing Authorization Header"), 401
+    errorMessage, errorCode = validateOrdersToDeliverRequest()
+    if len(errorMessage) > 0:
+        return jsonify(msg=errorMessage), errorCode
 
-    response = {
-        "orders": []
-    }
-    allOrders = Order.query.filter(Order.orderStatus == "CREATED").all()
-    for currentOrder in allOrders:
-        newDict = dict()
-        newDict["id"] = currentOrder.id
-        newDict["email"] = currentOrder.buyerEmail
-        response["orders"].append(newDict)
-
-    return jsonify(response), 200
+    return jsonify(getUndeliveredOrders()), 200
 
 
 @application.route("/pick_up_order", methods=["POST"])
 @jwt_required()
 def pick_up_order():
-    jwtToken = get_jwt()
-    if jwtToken["roleId"] != "3":
-        return jsonify(msg="Missing Authorization Header"), 401
+    errorMessage, errorCode, orderForPickUp = validatePickUpOrderRequest()
+    if errorCode == 401:
+        return jsonify(msg=errorMessage), errorCode
+    if len(errorMessage) > 0:
+        return jsonify(message=errorMessage), errorCode
 
-    orderId = request.json.get("id", "null")
-    if orderId == "null":
-        return jsonify(message="Missing order id."), 400
-    if type(orderId) is not int or orderId <= 0:
-        return jsonify(message="Invalid order id."), 400
-    if not Order.query.filter(Order.id == orderId).first():
-        return jsonify(message="Invalid order id."), 400
-    orderStatus = Order.query.filter(Order.id == orderId).first().orderStatus
-    if orderStatus == "PENDING" or orderStatus == "COMPLETE":
-        return jsonify(message="Invalid order id."), 400
-    order = Order.query.filter(Order.id == orderId).first()
-    order.orderStatus = "PENDING"
-    database.session.commit()
+    confirmOrderPickUp(orderForPickUp)
 
     return Response(status=200)
 
